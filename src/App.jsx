567f8e4c6501040a8bc10fdd5c886c67d4ec071c
Marcos Pividori <mpividori3@gmail.com>
2025-03-09 14:15:24 -0300

    add: deploy changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import {
   GsapFrom,
   GsapFromTo,
@@ -13,18 +12,23 @@ import {
 
 const App = () => {
   return (
-    <div className="bg-black min-h-screen w-full">
+    <div className="bg-black min-h-screen w-full flex flex-col">
       <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <header className="p-4 text-white">
+          <a href="#/" className="text-2xl font-bold">GSAP Animations</a>
+        </header>
+        <main className="flex-1 p-4 text-white">
+          <Routes>
+            <Route path="/gsapto" element={<GsapTo />} />
+            <Route path="/gsapfrom" element={<GsapFrom />} />
+            <Route path="/gsapfromto" element={<GsapFromTo />} />
+            <Route path="/gsaptimeline" element={<GsapTimeline />} />
+            <Route path="/gsapstagger" element={<GsapStagger />} />
+            <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
+            <Route path="/gsaptext" element={<GsapText />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
